4a5e10b5f39ca6b49901950e9f444a41afb31d6a
Tasya <137864082+tasya1220@users.noreply.github.com>
2023-07-04 19:10:06 +0700

    Add files via upload

---
+++ Week 2/script.js
@@ -0,0 +1,4 @@
+// membuat variabel nama untuk menampung isi elemt dari input user
+let nama = prompt("Masukan Nama Kamu");
+// menangkap tag id "nama" lalu menuliskan ke dalam tag id nama dengan input user
+document.getElementById("nama").innerHTML = nama;
